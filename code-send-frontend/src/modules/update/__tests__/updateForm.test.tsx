7d50c6bb9726bafc7afd5024a8cbbf0e493f7a33
M. Nindra Zaka <mnindrazaka@gmail.com>
2020-01-31 05:28:15 +0700

    feat(frontend/update): add update form

--- code-send-frontend/src/modules/update/__tests__/updateForm.test.tsx
+++ code-send-frontend/src/modules/update/__tests__/updateForm.test.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import '@testing-library/react/dont-cleanup-after-each'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
+import { Router } from 'react-router-dom'
+import { createMemoryHistory } from 'history'
 import UpdateForm from '../updateForm'
 
-const { container } = render(<UpdateForm />)
+const history = createMemoryHistory()
+const { getByPlaceholderText, getByText, findByTestId } = render(
+  <Router history={history}>
+    <UpdateForm />
+  </Router>
+)
 
-test('render correctly', async () => {
-  expect(container).toBeInTheDocument()
+test('show loading after submit', async () => {
+  const inputVersionElement = getByPlaceholderText('Enter Version')
+  const inputNoteElement = getByPlaceholderText('Enter Note')
+  const submitElement = getByText('Submit')
+
+  fireEvent.change(inputVersionElement, { target: { value: 'mockVersion' } })
+  fireEvent.change(inputNoteElement, { target: { value: 'mockNote' } })
+  fireEvent.click(submitElement)
+
+  const loadingElement = await findByTestId('loading')
+  expect(loadingElement).toBeInTheDocument()
 })
