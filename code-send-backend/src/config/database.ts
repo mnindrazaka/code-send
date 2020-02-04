7907c8e67e232cbaddb8262829289733d6ce5440
M. Nindra Zaka <mnindrazaka@gmail.com>
2020-02-04 11:21:06 +0700

    fix(backend/database): use environment variable for database connection

--- code-send-backend/src/config/database.ts
+++ code-send-backend/src/config/database.ts
@@ -1,12 +1,13 @@
 import mongoose from 'mongoose'
 import { MongoMemoryServer } from 'mongodb-memory-server'
 
+const { USERNAME, PASSWORD } = process.env
 const options = { useNewUrlParser: true, useUnifiedTopology: true }
 
 async function getConnectionString(isUsingMemory?: boolean) {
   return isUsingMemory
     ? await new MongoMemoryServer().getConnectionString()
-    : 'mongodb+srv://root:toor@cluster0-uzclu.mongodb.net/main?retryWrites=true&w=majority'
+    : `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0-uzclu.mongodb.net/main?retryWrites=true&w=majority`
 }
 
 export function connectDB(isUsingMemory?: boolean) {
