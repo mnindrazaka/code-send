d5ead7b7203c89b722c521072ea7f0a9e2956a79
M Nindra Zaka <mnindrazaka@gmail.com>
2020-02-04 11:22:14 +0700

    fix(backend/database): use environment variable for database connection

--- code-send-backend/src/server.ts
+++ code-send-backend/src/server.ts
@@ -1,6 +1,8 @@
+require('dotenv').config()
 import app from 'app'
 import { connectDB } from 'config/database'
 
+const port = process.env.PORT || 3000
 connectDB().then(() => {
-  app.listen(process.env.PORT || 3000, () => console.log('server running'))
+  app.listen(port, () => console.log(`server running on port ${port}`))
 })
