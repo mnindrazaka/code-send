b6f373c633d7a0b2870015b3794d25f3248cf1da
M Nindra Zaka <mnindrazaka@gmail.com>
2020-02-23 21:51:24 +0700

    feat(frontend/update): show update log

--- code-send-frontend/src/modules/update/__tests__/updateLog.test.tsx
+++ code-send-frontend/src/modules/update/__tests__/updateLog.test.tsx
@@ -1,7 +1,13 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, waitForDomChange } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import UpdateLog from "../updateLog";
+import codeSendService from "utils/api/codeSendService";
+
+jest.mock("utils/api/codeSendService");
+const codeSendServiceMock = codeSendService as jest.Mocked<
+  typeof codeSendService
+>;
 
 const renderUpdateLog = () => {
   const utils = render(
@@ -9,13 +15,27 @@ const renderUpdateLog = () => {
       <UpdateLog />
     </MemoryRouter>
   );
-
   return utils;
 };
 
 describe("update log", () => {
-  it("can render correctly", async () => {
-    const { container } = renderUpdateLog();
-    expect(container).toBeInTheDocument();
+  it("can render correct row count", async () => {
+    codeSendServiceMock.getAllUpdate.mockResolvedValueOnce([{}, {}, {}]);
+    const { getByTestId } = renderUpdateLog();
+    await waitForDomChange();
+    const tableBodyElement = getByTestId("table-body-update-log");
+    const tableRowElements = tableBodyElement.getElementsByTagName("tr");
+    expect(tableRowElements.length).toEqual(3);
+  });
+
+  it("can show failed message", async () => {
+    codeSendServiceMock.getAllUpdate.mockRejectedValueOnce({
+      status: "error",
+      message: "failed to get update"
+    });
+
+    const { findByText } = renderUpdateLog();
+    const alertElement = await findByText("Failed");
+    expect(alertElement).toBeInTheDocument();
   });
 });
--- code-send-frontend/src/utils/api/__tests__/codeSendService.test.ts
+++ code-send-frontend/src/utils/api/__tests__/codeSendService.test.ts
@@ -5,6 +5,11 @@ jest.mock("../service");
 const ServiceMock = Service as jest.MockedClass<typeof Service>;
 
 describe("code send service", () => {
+  it("can get all update", () => {
+    codeSendService.getAllUpdate();
+    expect(ServiceMock.mock.instances[0].get).toHaveBeenCalledWith("/update");
+  });
+
   it("can create update", () => {
     const update = { note: "mock note", version: "mock version" };
     codeSendService.createUpdate(update);
@@ -14,7 +19,7 @@ describe("code send service", () => {
     );
   });
 
-  it("can create update", () => {
+  it("can upload update", () => {
     const id = "mock id";
     const bundle = new Blob([]);
     const formData = new FormData();
