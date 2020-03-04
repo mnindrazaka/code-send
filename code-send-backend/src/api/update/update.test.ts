1c2155eb9100ea77ec1b9826c7afa2ba76e191e0
M Nindra Zaka <mnindrazaka@gmail.com>
2020-03-05 03:34:15 +0700

    fix(backend/update): faking database record for testing

--- code-send-backend/src/api/update/update.test.ts
+++ code-send-backend/src/api/update/update.test.ts
@@ -1,29 +1,42 @@
 import supertest from "supertest";
 import app from "app";
 import { expect } from "chai";
-import { connectDB } from "utils/database";
+import { connectDB, closeDB } from "utils/database";
+import updateModel from "./update.model";
+
 const request = supertest(app);
+const mockingDatabaseRecord = () => {
+  return updateModel.create({
+    version: "0.1",
+    note: "first release",
+    bundleUrl: "http://localhost"
+  });
+};
 
 describe("update", () => {
-  beforeAll(async () => await connectDB(true));
+  beforeEach(async () => {
+    await connectDB(true);
+    await mockingDatabaseRecord();
+  });
+  afterEach(async () => await closeDB(true));
 
-  test("create update", async () => {
-    const res = await request.post("/update").send({
-      version: "0.1",
-      note: "first release"
-    });
+  it("can get all update", async () => {
+    const res = await request.get("/update").send();
+    expect(res.body).to.be.an("array");
+  });
+
+  it("can get latest update", async () => {
+    const res = await request.get("/update/latest").send();
     expect(res.body).to.has.property("_id");
     expect(res.body).to.has.property("version");
     expect(res.body).to.has.property("note");
   });
 
-  test("get all update", async () => {
-    const res = await request.get("/update").send();
-    expect(res.body).to.be.an("array");
-  });
-
-  test("get latest update", async () => {
-    const res = await request.get("/update/latest").send();
+  it("can create update", async () => {
+    const res = await request.post("/update").send({
+      version: "0.1",
+      note: "first release"
+    });
     expect(res.body).to.has.property("_id");
     expect(res.body).to.has.property("version");
     expect(res.body).to.has.property("note");
