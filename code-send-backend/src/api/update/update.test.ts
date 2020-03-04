ed7d4e3585ef616c24ca137fc1dfcdbe50834d5a
M. Nindra Zaka <mnindrazaka@gmail.com>
2020-03-04 20:28:11 +0700

    fix(backend/update): fix test name

--- code-send-backend/src/api/update/update.test.ts
+++ code-send-backend/src/api/update/update.test.ts
@@ -3,8 +3,8 @@ import app from "app";
 import { expect } from "chai";
 import { connectDB, closeDB } from "utils/database";
 import updateModel from "./update.model";
-const request = supertest(app);
 
+const request = supertest(app);
 const mockingDatabaseRecord = () => {
   return updateModel.create({
     version: "0.1",
@@ -20,19 +20,19 @@ describe("update", () => {
   });
   afterEach(async () => await closeDB(true));
 
-  test("get all update", async () => {
+  it("can get all update", async () => {
     const res = await request.get("/update").send();
     expect(res.body).to.be.an("array");
   });
 
-  test("get latest update", async () => {
+  it("can get latest update", async () => {
     const res = await request.get("/update/latest").send();
     expect(res.body).to.has.property("_id");
     expect(res.body).to.has.property("version");
     expect(res.body).to.has.property("note");
   });
 
-  test("create update", async () => {
+  it("can create update", async () => {
     const res = await request.post("/update").send({
       version: "0.1",
       note: "first release"
