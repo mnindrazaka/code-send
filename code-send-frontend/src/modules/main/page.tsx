83b8d9624dda5bc260e1fcb6b82eb9021877f8ea
M. Nindra Zaka <mnindrazaka@gmail.com>
2020-03-08 21:03:34 +0700

    feat(frontend/project): fix navigation menu active state

--- code-send-frontend/src/modules/main/page.tsx
+++ code-send-frontend/src/modules/main/page.tsx
@@ -1,9 +1,60 @@
 import React from "react";
 import { Menu, Layout } from "antd";
 import { DashboardOutlined, GiftOutlined } from "@ant-design/icons";
-import { Route, Redirect, Link, Switch } from "react-router-dom";
+import { Route, Redirect, Link, Switch, useHistory } from "react-router-dom";
 import Dashboard from "modules/dashboard";
 import Update from "modules/update";
+import { NavigationMenuItem } from "interfaces/Navigation";
+
+const navigationMenuItems: NavigationMenuItem[] = [
+  {
+    path: "/dashboard",
+    component: Dashboard,
+    title: "Dashboard",
+    icon: <DashboardOutlined />
+  },
+  {
+    path: "/update",
+    component: Update,
+    title: "Update",
+    icon: <GiftOutlined />
+  }
+];
+
+const Navigation = () => {
+  const { location } = useHistory();
+
+  const selectedKeys = React.useMemo(() => {
+    const key = navigationMenuItems
+      .findIndex(({ path }) => location.pathname.includes(path))
+      .toString();
+    return [key];
+  }, [location.pathname]);
+
+  return (
+    <Menu style={{ height: "100%" }} selectedKeys={selectedKeys}>
+      {navigationMenuItems.map(({ path, title, icon }, index) => (
+        <Menu.Item key={index}>
+          <Link to={path}>
+            {icon}
+            {title}
+          </Link>
+        </Menu.Item>
+      ))}
+    </Menu>
+  );
+};
+
+const Content = () => {
+  return (
+    <Switch>
+      {navigationMenuItems.map(({ path, component }, index) => (
+        <Route key={index} path={path} component={component} />
+      ))}
+      <Redirect to={navigationMenuItems[0].path} />
+    </Switch>
+  );
+};
 
 const Page: React.FC = () => {
   return (
@@ -11,28 +62,10 @@ const Page: React.FC = () => {
       <Layout.Header>Code Send</Layout.Header>
       <Layout>
         <Layout.Sider>
-          <Menu style={{ height: "100%" }}>
-            <Menu.Item>
-              <Link to="/dashboard">
-                <DashboardOutlined />
-                Dashboard
-              </Link>
-            </Menu.Item>
-
-            <Menu.Item>
-              <Link to="/update">
-                <GiftOutlined />
-                Update
-              </Link>
-            </Menu.Item>
-          </Menu>
+          <Navigation />
         </Layout.Sider>
         <Layout.Content style={{ padding: "30px" }}>
-          <Switch>
-            <Route path="/dashboard" component={Dashboard} />
-            <Route path="/update" component={Update} />
-            <Redirect to="/dashboard" />
-          </Switch>
+          <Content />
         </Layout.Content>
       </Layout>
     </Layout>
