df57683e17da03463c459cf2c1702567bb0de6a6
M. Nindra Zaka <mnindrazaka@gmail.com>
2020-03-14 22:10:16 +0700

    chore(plugin/typescript): add typescript support

---
+++ code-send-plugin/lib/index.d.ts
@@ -0,0 +1,5 @@
+export declare function setActiveBundle(bundleId: string): void;
+export declare function registerBundle(bundleId: string, relativePath: string): void;
+export declare function reloadBundle(): void;
+export declare function getActiveBundle(): any;
+//# sourceMappingURL=index.d.ts.map
