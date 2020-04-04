0d165d203ad2bd20c2ad6dc659f068c4e4c46148
mnindrazaka <mnindrazaka@gmail.com>
2020-04-04 12:33:42 +0000

    Deploying to gh-pages from  @ 86720d7fb36d0aa49de22bc319b6e7369d6cc7e5 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/code-send/precache-manifest.d3d634279f33de8d50a07385252a8be4.js"
+  "/code-send/precache-manifest.a1ed12456ded919960dee521da61b0ab.js"
 );
 
 self.addEventListener('message', (event) => {
