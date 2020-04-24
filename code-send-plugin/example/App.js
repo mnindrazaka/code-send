42772e63ea15a107330be8119f88d876b9d25c49
M Nindra Zaka <mnindrazaka@gmail.com>
2020-04-24 20:20:57 +0700

    feat(plugin/example): add working example

--- code-send-plugin/example/App.js
+++ code-send-plugin/example/App.js
@@ -1,22 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { bundleManager, useCodeSend } from "code-send"
+import { useCodeSend } from "code-send"
 
 const App = () => {
-  // const {  } = useCodeSend("5e7fe2afa491f60003847d6b")
-  const { update } = useCheckUpdate("5e7fe2afa491f60003847d6b")
-  const { applyUpdate } = useApplyUpdate()
-
-  React.useEffect(() => {
-    console.log('update', update)
-    if (update) {
-      applyUpdate(update).then(() => {
-        bundleManager.getActiveBundle().then(bundle => {
-          console.log('bundle', bundle)
-        })
-      })
-    }
-  }, [update])
+  useCodeSend("5e7fe2afa491f60003847d6b")
   return (
     <View style={styles.container}>
       <Text style={styles.welcome}>CodeSend initial app</Text>
