0c2c85e1bc0bd1ec6b1b18d5901e4142d6bb973a
M Nindra Zaka <mnindrazaka@gmail.com>
2020-05-10 09:57:46 +0700

    feat(backend/update): add testing for check update service

--- code-send-backend/src/api/update/update.test.ts
+++ code-send-backend/src/api/update/update.test.ts
@@ -3,9 +3,13 @@ import supertest from "supertest";
 import app from "app";
 import { expect } from "chai";
 import { connectDB, closeDB, mockingDatabaseRecord } from "utils/database";
+import geocodingUtil from "api/geocoding/geocoding.util";
 
 process.env.JWT_SECRET = "secret";
 const request = supertest(app);
+
+jest.mock("api/geocoding/geocoding.util");
+const geocodingUtilMock = geocodingUtil as jest.Mocked<typeof geocodingUtil>;
 
 const authenticate = async () => {
   const authenticateResponse = await request
@@ -45,7 +49,7 @@ describe("update", () => {
       .send();
     expect(getAllUpdateResponse.body)
       .to.be.an("array")
-      .length(1);
+      .length(2);
   });
 
   it("can get latest update", async () => {
@@ -63,10 +67,10 @@ describe("update", () => {
     expect(getLatestUpdateResponse.body).to.has.property("_id");
     expect(getLatestUpdateResponse.body)
       .to.has.property("version")
-      .equal("0.1");
+      .equal("0.2");
     expect(getLatestUpdateResponse.body)
       .to.has.property("note")
-      .equal("first update");
+      .equal("second update");
     expect(getLatestUpdateResponse.body)
       .to.has.property("location")
       .to.has.property("latitude")
@@ -93,17 +97,17 @@ describe("update", () => {
       .post(`/project/${projectId}/update`)
       .set("authorization", `Bearer ${token}`)
       .send({
-        version: "0.2",
-        note: "second release"
+        version: "0.3",
+        note: "third release"
       });
 
     expect(createUpdateResponse.body).to.has.property("_id");
     expect(createUpdateResponse.body)
       .to.has.property("version")
-      .equal("0.2");
+      .equal("0.3");
     expect(createUpdateResponse.body)
       .to.has.property("note")
-      .equal("second release");
+      .equal("third release");
   });
 
   it("can edit update", async () => {
@@ -148,5 +152,129 @@ describe("update", () => {
     expect(editUpdateResponse.body)
       .to.has.property("message")
       .equal("update not found");
+  });
+
+  it("can get latest update if update is not regional update", async () => {
+    const token = await authenticate();
+    const getAllProjectResponse = await request
+      .get("/project")
+      .set("authorization", `Bearer ${token}`)
+      .send();
+    const projectId = getAllProjectResponse.body[0]._id;
+
+    await request
+      .post(`/project/${projectId}/update`)
+      .set("authorization", `Bearer ${token}`)
+      .send({
+        version: "0.3",
+        note: "third release",
+        bundleUrl: "http://localhost"
+      });
+
+    const getAllUpdateResponse = await request
+      .get(`/project/${projectId}/update`)
+      .set("authorization", `Bearer ${token}`)
+      .send();
+    const updateId = getAllUpdateResponse.body[0]._id;
+
+    const checkUpdateResponse = await request
+      .post(`/project/${projectId}/update/check`)
+      .set("authorization", `Bearer ${token}`)
+      .send({ updateId, latitude: -7.756928, longitude: 113.211502 });
+
+    expect(checkUpdateResponse.body)
+      .to.has.property("_id")
+      .equal(getAllUpdateResponse.body[2]._id);
+    expect(checkUpdateResponse.body)
+      .to.has.property("version")
+      .that.has.equal(getAllUpdateResponse.body[2].version);
+    expect(checkUpdateResponse.body)
+      .to.has.property("note")
+      .that.has.equal(getAllUpdateResponse.body[2].note);
+    expect(checkUpdateResponse.body)
+      .to.has.property("bundleUrl")
+      .that.has.equal(getAllUpdateResponse.body[2].bundleUrl);
+  });
+
+  it("can get latest update if in the same region when checking update", async () => {
+    geocodingUtilMock.reverse.mockResolvedValue("Jawa Timur, Indonesia");
+
+    const token = await authenticate();
+    const getAllProjectResponse = await request
+      .get("/project")
+      .set("authorization", `Bearer ${token}`)
+      .send();
+    const projectId = getAllProjectResponse.body[0]._id;
+
+    const getAllUpdateResponse = await request
+      .get(`/project/${projectId}/update`)
+      .set("authorization", `Bearer ${token}`)
+      .send();
+    const updateId = getAllUpdateResponse.body[0]._id;
+
+    const checkUpdateResponse = await request
+      .post(`/project/${projectId}/update/check`)
+      .set("authorization", `Bearer ${token}`)
+      .send({ updateId, latitude: -7.756928, longitude: 113.211502 });
+
+    expect(checkUpdateResponse.body)
+      .to.has.property("_id")
+      .equal(getAllUpdateResponse.body[1]._id);
+    expect(checkUpdateResponse.body)
+      .to.has.property("version")
+      .that.has.equal(getAllUpdateResponse.body[1].version);
+    expect(checkUpdateResponse.body)
+      .to.has.property("note")
+      .that.has.equal(getAllUpdateResponse.body[1].note);
+    expect(checkUpdateResponse.body)
+      .to.has.property("bundleUrl")
+      .that.has.equal(getAllUpdateResponse.body[1].bundleUrl);
+  });
+
+  it("can't get latest update if not in the same region when checking update", async () => {
+    geocodingUtilMock.reverse.mockResolvedValueOnce("Jakarta, Indonesia");
+    geocodingUtilMock.reverse.mockResolvedValueOnce("Jawa Timur, Indonesia");
+
+    const token = await authenticate();
+    const getAllProjectResponse = await request
+      .get("/project")
+      .set("authorization", `Bearer ${token}`)
+      .send();
+    const projectId = getAllProjectResponse.body[0]._id;
+
+    const getAllUpdateResponse = await request
+      .get(`/project/${projectId}/update`)
+      .set("authorization", `Bearer ${token}`)
+      .send();
+    const updateId = getAllUpdateResponse.body[0]._id;
+
+    const checkUpdateResponse = await request
+      .post(`/project/${projectId}/update/check`)
+      .set("authorization", `Bearer ${token}`)
+      .send({ updateId, latitude: -6.2, longitude: 106.816666 });
+
+    expect(checkUpdateResponse.body).to.not.has.property("_id");
+  });
+
+  it("can't get latest update if update is not newer when checking update", async () => {
+    const token = await authenticate();
+    const getAllProjectResponse = await request
+      .get("/project")
+      .set("authorization", `Bearer ${token}`)
+      .send();
+    const projectId = getAllProjectResponse.body[0]._id;
+
+    const getAllUpdateResponse = await request
+      .get(`/project/${projectId}/update`)
+      .set("authorization", `Bearer ${token}`)
+      .send();
+    const updateId = getAllUpdateResponse.body[1]._id;
+
+    const checkUpdateResponse = await request
+      .post(`/project/${projectId}/update/check`)
+      .set("authorization", `Bearer ${token}`)
+      .send({ updateId, latitude: -6.2, longitude: 106.816666 });
+
+    expect(checkUpdateResponse.body).to.not.has.property("_id");
   });
 });
