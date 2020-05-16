c62d69d23c2fb0d67f3c90c0d2bd221819eee050
M Nindra Zaka <mnindrazaka@gmail.com>
2020-05-17 04:22:36 +0700

    fix(frontend/update): add number validation to update version

--- code-send-frontend/src/components/formikWrapper/textField.tsx
+++ code-send-frontend/src/components/formikWrapper/textField.tsx
@@ -5,21 +5,29 @@ import { useField } from "formik";
 interface TextFieldProps {
   label: string;
   name: string;
+  placeholder?: string;
   type?: "text" | "password";
 }
 
-export const TextField: React.FC<TextFieldProps> = ({ name, label, type }) => {
+export const TextField: React.FC<TextFieldProps> = ({
+  name,
+  label,
+  placeholder,
+  type
+}) => {
   const [field, meta] = useField({ name });
   const renderInput = useCallback(() => {
     switch (type) {
       case "text":
-        return <Input {...field} id={name} />;
+        return <Input {...field} id={name} placeholder={placeholder} />;
       case "password":
-        return <Input.Password {...field} id={name} />;
+        return (
+          <Input.Password {...field} id={name} placeholder={placeholder} />
+        );
       default:
-        return <Input {...field} id={name} />;
+        return <Input {...field} id={name} placeholder={placeholder} />;
     }
-  }, [type, name, field]);
+  }, [type, name, placeholder, field]);
   return (
     <Form.Item
       label={label}
